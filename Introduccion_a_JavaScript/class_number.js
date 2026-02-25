22c308933e750f8b93c68a6254c688e0d94cfa38
franz yahuira <franzyahuira@gmail.com>
2026-02-25 12:04:05 -0500

    introduccion a JavaScript

---
+++ Introduccion_a_JavaScript/class_number.js
@@ -0,0 +1,37 @@
+// 1. Tipo entero y decimal
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+// 2. Notacion cientifica
+const cientifico = 5e3
+
+// 3. Infinitos y NaN
+const infinito =Infinity
+const noEsUnNumero = NaN
+
+// Operaciones aritmeticas
+
+// 1. Suma, Resta, Multicacion y divicion
+const suma = 3 + 4
+const resta  = 4 - 4
+const multicacion = 4 * 7
+const divicion = 16 / 2
+
+// 2. Modulo y Explonenciacion
+const modulo = 15 % 8
+const explonenciacion = 2 ** 3
+
+// Presicion
+const resultado = 0.1 + 0.2 
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado === 0.3)
+
+// Operaciones Avansadas
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
