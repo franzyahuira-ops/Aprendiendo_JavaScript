a9508a9daff045f7f157b75591af6d7719bf7869
franz yahuira <franzyahuira@gmail.com>
2026-02-25 10:59:03 -0500

    nuevos cambios

---
+++ Curso_de_JavaScript/strings.js
@@ -0,0 +1,18 @@
+//tipo de dato: string  
+
+let string1 = 'Hola,mundo'
+let string2 = "JavaScript es genial"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es Extremadamente Genial'
+console.log(frase.length)
+console.log(frase.toLocaleLowerCase())
+console.log(frase.toLocaleUpperCase())
+console.log(frase.substring(0, 10))
